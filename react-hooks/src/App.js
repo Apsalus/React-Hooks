c6397c21f448e74441b3d31341556a623c46fa7e
dongjun <allnight724@gmail.com>
2023-11-03 19:15:53 +0900

    useInput

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,21 +1,4 @@
-import React, { useState } from 'react';
-
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = event => {
-    const {
-      target: { value }
-      }  = event;
-      let willUpdate = true;
-      if(typeof validator === "function"){
-        willUpdate = validator(value);
-      }
-      if(willUpdate){
-        setValue(value);
-      }
-  };
-  return { value , onChange };
-};
+import { useInput } from './useInput';
 
 const App = () => {
   const maxLen = (value) => !value.includes("@");

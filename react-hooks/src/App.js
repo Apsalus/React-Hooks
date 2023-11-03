5fb5d29248e14031ba1d8502c8775136e62e9ee1
dongjun <allnight724@gmail.com>
2023-11-03 17:38:11 +0900

    useInput

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,52 +1,31 @@
-import { Component, useState } from "react";
+import React, { useState } from 'react';
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+      }  = event;
+      let willUpdate = true;
+      if(typeof validator === "function"){
+        willUpdate = validator(value);
+      }
+      if(willUpdate){
+        setValue(value);
+      }
+  };
+  return { value , onChange };
+};
 
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>start edting to see some magic happen!</h2>
-      <button onClick={incrementItem}> Increment</button>
-      <button onClick={decrementItem}> Decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
-
-//리액트 훅이 나온 이유, 클래스와 차이점, 간단한 사용법
-
-/*
-class AppUgly extends React.Component{
-  state = {
-    item: 1
-  }
-  render(){
-    const { item } = this.state;
-    return(
-      <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>start edting to see some magic happen!</h2>
-      <button onClick={this.incrementItem}> Increment</button>
-      <button onClick={this.decrementItem}> Decrement</button>
-    </div>
-    );
-  }
-  incrementItem = () => {
-    this.setItem(state => {
-      return {
-        item: state.item +1
-      }
-    })
-  }
-  decrementItem = () =>{
-    this.setItem(state => {
-      return {
-        item: state.item -1
-      }
-    })
-  }
-}
-*/
 
 export default App;
